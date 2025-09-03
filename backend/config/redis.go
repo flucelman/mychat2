f44869abff37b4b005b0819c3e84a6636c8b645e
flucelman <2441730566@qq.com>
2025-09-03 08:12:52 +0800

    chore: go编译

--- backend/config/redis.go
+++ backend/config/redis.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"backend/global"
+	"os"
 
 	"github.com/go-redis/redis"
 )
@@ -10,7 +11,7 @@ func InitRedis() {
 	global.RedisClient = redis.NewClient(&redis.Options{
 		Addr:     AppConfig.Redis.Host + ":" + AppConfig.Redis.Port,
 		DB:       AppConfig.Redis.DB,
-		Password: AppConfig.Redis.Password,
+		Password: os.Getenv("REDIS_PASSWORD"),
 	})
 
 	_, err := global.RedisClient.Ping().Result()
